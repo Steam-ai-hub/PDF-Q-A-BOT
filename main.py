b890e451b9a24ace2601a710bc366bcc4edb6626
Steam-ai-hub <aimicentauri@gmail.com>
2025-06-23 15:26:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import sqlite3import streamlit as st
+import sqlite3
+import streamlit as st
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_chroma import Chroma
